2a74002e5b1fd4b10da1bce9a9ae56e4a70ddf12
AVISHERE <akashvyas2006@gmail.com>
2024-09-05 12:48:57 +0530

    searchbar updated

--- frontend/src/components/productsList.js
+++ frontend/src/components/productsList.js
@@ -3,39 +3,72 @@ import { Link } from "react-router-dom";
 
 const ProductList = () =>{
     const [products, setProducts] = useState([])
+    const [searchText, setSearchText] = useState("")
+    useEffect(()=>{
+        if(searchText !== ""){
+        setTimeout(() => {
+                searchProduct();
+            }, 500);
+        }else{
+            showProducts()
+        }
+    },[searchText])
     useEffect(() => {
         showProducts()
     }, [])
-    const updateProduct = async () =>{
-        console.log("hello world");
-        
+    const searchProduct = async () =>{
+   if (searchText.trim()!=="") {
+         const data = await fetch(`http://localhost:5000/v1/product/search-product/${searchText}`)//serach the product in the database and it will show resul
+         const result = await data.json();//data is converted into string 
+         setProducts(result);//update the propduct list
+   }
+   else{
+    showProducts()
+   }
     }
     const showProducts = async () =>{
         const data = await fetch("http://localhost:5000/v1/product/products");
         const result =  await data.json();
         setProducts(result)
-        
+    }
+    const deleteProduct = async (id) =>{
+        let confirmation = window.confirm("delete the product?");
+        if(confirmation){
+        const data = await fetch(`http://localhost:5000/v1/product/delete-product/${id}`,{method:"DELETE"});
+        const result = await data.json();
+        console.log(result);
+        if(result){
+            alert("product deleted")
+            showProducts()
+        }
+        }
+        else{
+            return false;
+        }
     }
     
     return (
         <>
-        <div>Products</div>
+        <div className="searchBar">
+        <input type="text" placeholder="search your product here" onChange={(e)=>setSearchText(e.target.value)}/>
+        <button onClick={searchProduct}>search</button>
+        </div>
         <ul className="products-ul" style={{marginRight:'120px',color:'red'}}>
             <li>product name</li>
             <li>product price</li>
             <li>product category</li>
             <li>product company</li>
         </ul>
-        {products.map((e)=>
+        {products.length > 0?products.map((e)=>
         <ul className="products-ul">
             <li>{e.name}</li>
             <li>{e.price}</li>
             <li>{e.category}</li>
             <li>{e.company}</li>
-            <button onClick={updateProduct}><Link to={'/update/'+e._id}>Update</Link></button>&nbsp;&nbsp;
-            <button><Link>Delete</Link></button>
+            <button><Link to={'/update/'+e._id}>Update</Link></button>&nbsp;&nbsp;
+            <button onClick={()=>deleteProduct(e._id)}>Delete</button>
         </ul>
-        )}
+        ):<p>nothing found</p>}
         </>
     )
 }
